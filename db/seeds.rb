6cd616e08ddab7b0651099e8e5e26992a7f493c8
Troy Chryssos <github email address>
2016-10-05 11:23:04 -0400

    Got twitch API working, working through fetch runners on spedrun API

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,39 +40,39 @@
 #   category.save
 # end
 
-mario64=Game.create(name: "Super Mario 64", artwork_url: "http://www.mobygames.com/images/covers/l/216304-super-mario-64-nintendo-64-front-cover.jpg")
-
-oot=Game.create(name: "The Legend of Zelda: Ocarina of Time", artwork_url:"http://cdn.playbuzz.com/cdn/a9a90ae4-de77-4549-a76a-9280d3cd9868/a3a6d061-0541-4112-9817-23dd83836fb7.png")
-
-cheese=Runner.create(username:"cheese05", stream_url:"https://www.twitch.tv/cheese05")
-
-puncay=Runner.create(username:"puncayshun", stream_url:"https://www.twitch.tv/puncayshun")
-
-skater=Runner.create(username:"skater82297", stream_url:"https://www.twitch.tv/skater82297")
-
-torje=Runner.create(username:"torjeagc", stream_url:"https://www.twitch.tv/torjeagc")
-
-mario120star=CategoryLeaderboard.create(placements: "8m73rgwy,1zqjp35m,3,4,5,6,7,8,9,10", game_id:1, title:"120 Star", rules: "Complete the game while collecting every star. No restrictions. All forms of BLJ are allowed.
-
-Timing starts from reset and ends when the big star is collected at the end of the game.")
-
-ootany=CategoryLeaderboard.create(placements: "8m7ow6wy,9yo05q1y,3,4,5,6,7,8,9,10", game_id:2, title: "Any %", rules: "Any% Rules:
--Reach the 'The End' screen, no additional restrictions.
--Timing ends at the start of the Ganon death cutscene, or upon the credits scene loading (if done via wrongwarp).
-
-Ocarina of Time Rules
--Timing starts when selecting 'Yes' on the file select screen
--1.0/1.1 Virtual Console or GameCube injects are banned
-
-Emulator-specific rules:
--You must use Project64 1.6 or 1.7. All other emulators and versions (such as Mupen64 and Project64 2.0+) are banned.
--You must use the default settings for emulation.
--Tricks and glitches exclusive to 1.0/1.1 cannot be used on emulator to save time.")
-
-mario_first=Run.create(speedrun_id:"8m73rgwy", runner_id:1, category_leaderboard_id: 1, run_url:"https://www.youtube.com/watch?v=EfS81QvdbF8")
-
-mario_second=Run.create(speedrun_id:"1zqjp35m", runner_id:2, category_leaderboard_id: 1, run_url:"https://www.youtube.com/watch?v=f_lXYIUf3KE")
-
-zelda_first=Run.create(speedrun_id:"8m7ow6wy", runner_id:3, category_leaderboard_id: 2, run_url:"https://www.youtube.com/watch?v=9BGE9H7iPx8")
-
-zelda_second=Run.create(speedrun_id:"9yo05q1y", runner_id:4, category_leaderboard_id: 2, run_url:"https://www.youtube.com/watch?v=HGa-mhS4YDY")
+# mario64=Game.create(name: "Super Mario 64", artwork_url: "http://www.mobygames.com/images/covers/l/216304-super-mario-64-nintendo-64-front-cover.jpg")
+#
+# oot=Game.create(name: "The Legend of Zelda: Ocarina of Time", artwork_url:"http://cdn.playbuzz.com/cdn/a9a90ae4-de77-4549-a76a-9280d3cd9868/a3a6d061-0541-4112-9817-23dd83836fb7.png")
+#
+# cheese=Runner.create(username:"cheese05", stream_url:"https://www.twitch.tv/cheese05")
+#
+# puncay=Runner.create(username:"puncayshun", stream_url:"https://www.twitch.tv/puncayshun")
+#
+# skater=Runner.create(username:"skater82297", stream_url:"https://www.twitch.tv/skater82297")
+#
+# torje=Runner.create(username:"torjeagc", stream_url:"https://www.twitch.tv/torjeagc")
+#
+# mario120star=CategoryLeaderboard.create(placements: "8m73rgwy,1zqjp35m,3,4,5,6,7,8,9,10", game_id:1, title:"120 Star", rules: "Complete the game while collecting every star. No restrictions. All forms of BLJ are allowed.
+#
+# Timing starts from reset and ends when the big star is collected at the end of the game.")
+#
+# ootany=CategoryLeaderboard.create(placements: "8m7ow6wy,9yo05q1y,3,4,5,6,7,8,9,10", game_id:2, title: "Any %", rules: "Any% Rules:
+# -Reach the 'The End' screen, no additional restrictions.
+# -Timing ends at the start of the Ganon death cutscene, or upon the credits scene loading (if done via wrongwarp).
+#
+# Ocarina of Time Rules
+# -Timing starts when selecting 'Yes' on the file select screen
+# -1.0/1.1 Virtual Console or GameCube injects are banned
+#
+# Emulator-specific rules:
+# -You must use Project64 1.6 or 1.7. All other emulators and versions (such as Mupen64 and Project64 2.0+) are banned.
+# -You must use the default settings for emulation.
+# -Tricks and glitches exclusive to 1.0/1.1 cannot be used on emulator to save time.")
+#
+# mario_first=Run.create(speedrun_id:"8m73rgwy", runner_id:1, category_leaderboard_id: 1, run_url:"https://www.youtube.com/watch?v=EfS81QvdbF8")
+#
+# mario_second=Run.create(speedrun_id:"1zqjp35m", runner_id:2, category_leaderboard_id: 1, run_url:"https://www.youtube.com/watch?v=f_lXYIUf3KE")
+#
+# zelda_first=Run.create(speedrun_id:"8m7ow6wy", runner_id:3, category_leaderboard_id: 2, run_url:"https://www.youtube.com/watch?v=9BGE9H7iPx8")
+#
+# zelda_second=Run.create(speedrun_id:"9yo05q1y", runner_id:4, category_leaderboard_id: 2, run_url:"https://www.youtube.com/watch?v=HGa-mhS4YDY")
