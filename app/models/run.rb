a72b6e7f586a0b1991bc7802b89aea44e4f2052e
github username <github email address>
2016-10-05 13:11:11 -0400

    Added Run serailizer attrs

--- app/models/run.rb
+++ app/models/run.rb
@@ -5,14 +5,14 @@ class Run < ApplicationRecord
   has_many :saved_runs
 
   def run_placement
-    placed_runs_hash={}
-    CategoryLeaderboard.all.each do |leaderboard|
+    placed_runs_hash=[]
+    CategoryLeaderboard.all.each_with_index do |leaderboard, i|
       placements=leaderboard.placements.split(",")
       if placements.find{|placed_run| placed_run===self.speedrun_id}
         game_name=leaderboard.game.name
         category=leaderboard.title
         placing=placements.index(self.speedrun_id)+1
-        placed_runs_hash[game_name]={category=>placing}
+        placed_runs_hash[i]={'name': game_name,'category': category,'place': placing}
       end
     end
   return placed_runs_hash
