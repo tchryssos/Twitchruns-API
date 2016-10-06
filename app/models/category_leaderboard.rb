59f52fa3194fa788b46d648afa1126227a134497
Troy Chryssos <github email address>
2016-10-06 17:38:07 -0400

    switched placement list to display full runs rather than IDs

--- app/models/category_leaderboard.rb
+++ app/models/category_leaderboard.rb
@@ -8,7 +8,7 @@ class CategoryLeaderboard < ApplicationRecord
     split_placements=self.placements.split(",")
     new_placements_list={}
     split_placements.each_with_index do |place, i|
-      new_placements_list[i+1]=place
+      new_placements_list[i+1]=Run.find_by(speedrun_id: place)
     end
     return new_placements_list
   end
