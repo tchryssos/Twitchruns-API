6bf616e767728d2e297416582d255a2824861a6a
Troy Chryssos <github email address>
2016-10-13 13:19:17 -0400

    added thumbnail to serializer

--- app/serializers/run_serializer.rb
+++ app/serializers/run_serializer.rb
@@ -1,5 +1,5 @@
 class RunSerializer<ActiveModel::Serializer
-  attributes :speedrun_id, :id, :run_url, :run_placement, :runner_id, :place
+  attributes :speedrun_id, :id, :run_url, :run_placement, :runner_id, :place, :thumbnail
   has_one :runner
   has_one :game
   has_one :category_leaderboard
